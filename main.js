d7389682ffc28c67c5b648e0d03977a14fc03e04
Nicolas <contact@nicolas-devynck.fr>
2020-08-13 19:18:47 +0200

    changement complet de style

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 380,
+    height: 625,
 //    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  // mainWindow.removeMenu()
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
