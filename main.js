994b602cb12d12a9399693bd3b0e91234d99af09
Nicolas <contact@nicolas-devynck.fr>
2020-08-14 01:34:50 +0200

    division des page, supression des animation

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  // mainWindow.removeMenu()
+  mainWindow.removeMenu()
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+   mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
